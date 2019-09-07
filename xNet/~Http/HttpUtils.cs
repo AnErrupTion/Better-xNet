f0645484efcf3b140082d469630d411df16f90dc
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-09-07 14:33:20 +0200

    3.3.4

---
+++ xNet/~Http/HttpUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Better_xNet
+{
+    public class HttpUtils
+    {
+        public static string GetPageSource(string address)
+        {
+            return new WebClient().DownloadString(address);
+        }
+
+        public static string GetPageSource(Uri address)
+        {
+            return new WebClient().DownloadString(address);
+        }
+    }
+}
