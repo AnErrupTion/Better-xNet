400d322062771e99df88db54f689a5dcec86c7e7
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-09-07 14:29:34 +0200

    3.3.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
@@ -6,19 +6,19 @@ using System.Resources;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("xNet")]
-[assembly: AssemblyDescription("https://github.com/X-rus/xNet")]
+[assembly: AssemblyTitle("Better-xNet")]
+[assembly: AssemblyDescription("A fork of the original xNet libary.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ruslan Khuduev")]
-[assembly: AssemblyProduct("xNet")]
-[assembly: AssemblyCopyright("Copyright © 2012-2016 Ruslan Khuduev <x-rus@list.ru>")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Better-xNet")]
+[assembly: AssemblyCopyright("© AnErrupTion 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
 // для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
 // COM, задайте атрибуту ComVisible значение TRUE для этого типа.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("3df078d0-a043-4e10-86da-40d19b11142b")]
@@ -33,6 +33,6 @@ using System.Resources;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.3.0")]
-[assembly: AssemblyFileVersion("3.3.3.0")]
-[assembly: NeutralResourcesLanguageAttribute("ru")]
+[assembly: AssemblyVersion("3.3.3.4")]
+[assembly: AssemblyFileVersion("3.3.3.4")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
