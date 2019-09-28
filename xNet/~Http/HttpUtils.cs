37457a84a54168841f54147c0c4f1d5c14d83e8b
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-09-28 18:47:01 +0200

    3.3.7

--- xNet/~Http/HttpUtils.cs
+++ xNet/~Http/HttpUtils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +6,68 @@ using System.Net;
 
 namespace Better_xNet
 {
-    public class HttpUtils
+    public static class HttpUtils
     {
-        public static string GetPageSource(string address)
+        public static string GetPageSource(string address, string ua)
         {
-            return new WebClient().DownloadString(address);
+            using (HttpRequest req = new HttpRequest())
+            {
+                req.UserAgent = ua;
+                req.KeepAlive = true;
+                req.Cookies = new CookieStorage();
+
+                string respo = req.Get(address).ToString();
+                return respo;
+            }
+
+            return null;
         }
 
-        public static string GetPageSource(Uri address)
+        public static string GetPageSource(Uri address, string ua)
         {
-            return new WebClient().DownloadString(address);
+            using (HttpRequest req = new HttpRequest())
+            {
+                req.UserAgent = ua;
+                req.KeepAlive = true;
+                req.Cookies = new CookieStorage();
+
+                string respo = req.Get(address).ToString();
+                return respo;
+            }
+
+            return null;
+        }
+
+        public static bool TestWebsite(string address, string ua, int timeout = 10000)
+        {
+            using (HttpRequest req = new HttpRequest())
+            {
+                req.UserAgent = ua;
+                req.KeepAlive = true;
+                req.Cookies = new CookieStorage();
+                req.ConnectTimeout = timeout;
+
+                bool respo = req.Get(address).IsOK;
+                if (respo) return true;
+            }
+
+            return false;
+        }
+
+        public static bool TestWebsite(Uri address, string ua, int timeout = 10000)
+        {
+            using (HttpRequest req = new HttpRequest())
+            {
+                req.UserAgent = ua;
+                req.KeepAlive = true;
+                req.Cookies = new CookieStorage();
+                req.ConnectTimeout = timeout;
+
+                bool respo = req.Get(address).IsOK;
+                if (respo) return true;
+            }
+
+            return false;
         }
     }
 }
