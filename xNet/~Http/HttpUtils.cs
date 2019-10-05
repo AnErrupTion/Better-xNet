a0ea4fedb5f289666d67ce0e7fa7c08d41b6528c
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-10-05 12:12:04 +0200

    Update HttpUtils.cs

--- xNet/~Http/HttpUtils.cs
+++ xNet/~Http/HttpUtils.cs
@@ -8,20 +8,6 @@ namespace Better_xNet
 {
     public static class HttpUtils
     {
-        public static string GetPageSource(string address, string ua)
-        {
-            using (HttpRequest req = new HttpRequest())
-            {
-                req.UserAgent = ua;
-                req.KeepAlive = true;
-                req.Cookies = new CookieStorage();
-
-                string respo = req.Get(address).ToString();
-                return respo;
-            }
-
-            return null;
-        }
 
         public static string GetPageSource(Uri address, string ua)
         {
@@ -36,22 +22,6 @@ namespace Better_xNet
             }
 
             return null;
-        }
-
-        public static bool TestWebsite(string address, string ua, int timeout = 10000)
-        {
-            using (HttpRequest req = new HttpRequest())
-            {
-                req.UserAgent = ua;
-                req.KeepAlive = true;
-                req.Cookies = new CookieStorage();
-                req.ConnectTimeout = timeout;
-
-                bool respo = req.Get(address).IsOK;
-                if (respo) return true;
-            }
-
-            return false;
         }
 
         public static bool TestWebsite(Uri address, string ua, int timeout = 10000)
